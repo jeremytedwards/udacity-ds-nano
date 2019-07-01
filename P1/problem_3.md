4f9205b6cfa6545e7ac0ec28f43c2793469534f5
Jeremy Edwards <jeremytedwards@gmail.com>
2019-07-01 00:20:16 -0700

    Worked through Problem 3 and updated the description.

--- P1/problem_3.md
+++ P1/problem_3.md
@@ -1,4 +1,25 @@
 Problem 3: Huffman Coding
 ------------
 
-... needs more work ... looking for a review
+A Huffman Coding is method of compressing by computing an optimal number of bits for a 
+given input taking advantage of frequency of each input. In our case we're working with 
+strings.
+
+The steps to accomplish this were:
+ 
+ Encoding:
+ 1. Build list of tuples representing the frequency of each character, using Python 
+ collections.counter(), with uses the subclass of dict. _Constructing it is usually O(n)_.
+ 2. Sort the list and convert to a list representation of a binary tree, frequencies(total)
+ as the nodes and frequency + characters at the leaves. 
+ 3. Following that trim the frequencies and assigned the codes by walking the object.
+ "0" for left and "1" for right.
+
+_(If the symbols are sorted by probability, there is a linear-time O(n) method to 
+create the Huffman tree, O(log n) average lookup)_
+
+ Decoding:
+ 1. Take the tree representation and the frequency bits and walk the tree till a character
+ is found, starting over after each find. _O(log n) average lookup_.
+
+
--- P1/problem_3.py
+++ P1/problem_3.py
@@ -4,128 +4,104 @@ import sys
 from collections import Counter
 
 
-class Node(object):
-    """Create Node class."""
+def build_tree(tuple_list):
+    while len(tuple_list) > 1:
+        tuple_list.sort(key=lambda tup: tup[0])  # https://docs.python.org/3/howto/sorting.html
+        lowest_two = tuple_list[0:2]
+        remaining_tuples = tuple_list[2:]
+        freq_total = lowest_two[0][0] + lowest_two[1][0]
+        tuple_list = remaining_tuples + [(freq_total, lowest_two)]
 
-    def __init__(self, tup=(None, 0)):
-        """Init Node."""
-        self._left = None
-        self._right = None
-        self.data = tup[0]
-        self.code = ""
-        self.freq = tup[1]
-
-    def __repr__(self):
-        """Return a string which when eval'ed will rebuild tree"""
-        return '{}({}:{}:{}, {}, {})'.format(
-            self.__class__.__name__,
-            repr(self.data),
-            repr(self.freq),
-            repr(self.code),
-            repr(self._left) if self._left else None,
-            repr(self._right) if self._right else None) \
-            .replace(', None, None)', ')') \
-            .replace(', None)', ')')
-
-    def set_left(self, l):
-        self._left = l
-
-    def set_right(self, r):
-        self._right = r
-
-    def set_freq(self, f):
-        self.freq = f
-
-    def pre_order(self, code=""):
-        """Returns the data and freq of all nodes in pre-order
-        (me, all left, all right)"""
-        # Return me
-        self.code = code
-        yield self.data, self.freq, self.code
-        # Return all the _left items
-        if self._left:
-            code += "0"
-            for d, f, c in self._left.pre_order(code):
-                yield d, f, c
-        # Return all the _right
-        if self._right:
-            code += "1"
-            for d, f, c in self._right.pre_order(code):
-                yield d, f, c
+    return tuple_list[0]   # Return the single tree inside the list
 
 
-def huffman_sub(left_node, right_node):
-    top = Node()
-    top._left = left_node
-    top._right = right_node
-    top.freq = top._left.freq + top._right.freq
-    return top
+def trim_tree(tree):
+    # Trim the freq counters off, leaving just the letters
+    p = tree[1]
+    if isinstance(p, str):
+        return p   # if it's a leaf return it
+    else:
+        return trim_tree(p[0]), trim_tree(p[1])
 
 
-def huffman_encoding(data):
+def assign_codes(node, pat=''):
+    global codes
+    hm_codes = ""
+    if isinstance(node, str):
+        codes[node] = pat   # A leaf. Set its code
+        hm_codes += pat
+    else:
+        assign_codes(node[0], pat+"0")   # do left branch...
+        assign_codes(node[1], pat+"1")   # then do the right branch
+    return hm_codes
+
+
+def build_huffman_tree(data):
     """
     Takes a string and counts the frequency of the characters, converts those into
-    a Trie with weight and combines Trie's smallest to largest.
+    a Trie with weight and combines Trie's smallest to largest. Finally is sets
+    the codes in the GLOBAL variable 'codes' in __main__
     :param data:
     :return: a Trie
     """
-    ordered_data = Counter(data).most_common()
-    hm = []
+    frequency_data = Counter(data).items()
 
-    # First pass create the leaves
-    while ordered_data:
-        sub_left_node = Node(ordered_data.pop())
-        if ordered_data:
-            sub_right_node = Node(ordered_data.pop())
-            hm.append(huffman_sub(sub_left_node, sub_right_node))
+    # Reverse the (k, v) mapping
+    frequency_data = [(v, k) for k, v in frequency_data]
+
+    # build the tree for encoding
+    huffman_tree = build_tree(frequency_data)
+    huffman_tree = trim_tree(huffman_tree)
+
+    # Set the codes in global variable "codes"
+    hm_codes = assign_codes(huffman_tree)
+
+    return hm_codes, huffman_tree
+
+
+def huffman_encoding(data):
+    tree = build_huffman_tree(data)
+
+    global codes
+    output = ""
+    for ch in data:
+        output += codes[ch]
+
+    return output, tree
+
+
+def huffman_decoding(data, tree):
+    output = ""
+    p = tree[1]
+    for bit in data:
+        if bit == '0':
+            p = p[0]   # Go left
         else:
-            hm.append(sub_left_node)
-
-    # compare the frequencies and combine the leaves
-    while len(hm) > 1:
-        sorted(hm, key=lambda item: item.freq)
-        if hm[0].freq <= hm[1].freq:
-            hm.append(huffman_sub(hm.pop(), hm.pop()))
-        else:
-            hm.append(huffman_sub(hm.pop(1), hm.pop()))
-
-    od = [c for d, f, c in hm[0].pre_order() if c is not ""]
-
-    return od, hm[0]
-
-
-def huffman_decoding(hm_tree):
-    decoded = ""
-    decoded_list = [d*int(f) for d, f, c in hm_tree.pre_order() if d is not None]
-    for i in decoded_list:
-        decoded += i
-    return decoded
+            p = p[1]   # Go right
+        if isinstance(p, str):
+            output += p   # found character, add to output
+            p = tree[1]   # back to top
+    return output
 
 
 if __name__ == "__main__":
     codes = {}
 
-    a_great_sentence = "The bird is the word"
     # a_great_sentence = "cccabbdddd"
+    # a_great_sentence = "THE BIRD IS THE WORD"
+    # a_great_sentence = "The bird is the word"
+    # a_great_sentence = "3332114444"
+    a_great_sentence = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"
 
     print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
     print("The content of the data is: {}\n".format(a_great_sentence))
 
-    ed_list, hm_tree = huffman_encoding(a_great_sentence)
-
-    encoded_data = ""
-    for i in ed_list:
-        encoded_data += i
+    encoded_data, hm_tree = huffman_encoding(a_great_sentence)
 
     print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
     print("The content of the encoded data is: {}\n".format(encoded_data))
 
-    # used for testing
-    # print(ed_list)
-    # print(hm_tree)
-    # print("\n")
-
-    decoded_data = huffman_decoding(hm_tree)
+    decoded_data = huffman_decoding(encoded_data, hm_tree)
 
     print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    print("The content of the encoded data is: {}\n".format(decoded_data))
+    print("The content of the decoded data is: {}\n".format(decoded_data))
